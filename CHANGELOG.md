9ceb7c169005931afd7dbc37b5a3868884a07645
Kelvin Ng <kelvinng@interbase.com.my>
2021-01-29 10:23:40 +0800

    add documentation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
-## 0.1.0
+
+## [Unreleased]
+
+- Added documentation.
+
+## [0.1.0] - 2021-01-29
 
 - Initial release.
--- lib/huawei_in_app_update.dart
+++ lib/huawei_in_app_update.dart
@@ -6,6 +6,15 @@ class HuaweiInAppUpdate {
   static const MethodChannel _channel =
       const MethodChannel('huawei_in_app_update');
 
+  /// Initiate to check for update from Huawei Server
+  /// and retrieve App Update information
+  ///
+  /// Returns [UpgradeInfo].
+  ///
+  /// If no update available, [UpgradeInfo.updateAvailable]
+  /// will return false.
+  ///
+  /// Throws a [PlatformException] if error occured.
   static Future<UpgradeInfo> checkForUpdate() async {
     try {
       final info = await _channel.invokeMethod('checkForUpdate');
@@ -46,6 +55,9 @@ class HuaweiInAppUpdate {
     }
   }
 
+  /// Display a update dialog provided by Huawei API.
+  ///
+  /// Throws a [PlatformException] if error occured.
   static void showUpdateDialog() async {
     try {
       await _channel.invokeMethod('showUpdateDialog');
@@ -56,30 +68,83 @@ class HuaweiInAppUpdate {
 }
 
 class UpgradeInfo {
+  /// Whether new update is available.
   final bool updateAvailable;
+
+  /// App ID
   final String appId;
+
+  /// App name
   final String appName;
+
+  /// Package name parsed from the APK.
   final String packageName;
+
+  /// Size of the differential package.
   final int diffSize;
+
   final String diffDownUrl;
+
   final String diffSha2;
+
+  /// Indicates whether the signature is different. 
+  /// 
+  /// The options are as follows:
+  /// - **0**: no
+  /// - **1**: yes
   final int sameS;
+
+  /// Size of the app of the target version.
   final int size;
+
+  /// App release time, accurate to day. The value is in the yyyy-MM-dd format.
   final String releaseDate;
+
+  /// URL of the app icon
   final String icon;
+
+  /// Internal version number parsed from the APK.
   final int versionCode;
+
+  /// Target version name.
   final String versionName;
+
+  /// Source version number.
   final int oldVersionCode;
+
+  /// Source version name.
   final String oldVersionName;
+
+  /// URL for downloading the complete or differential app package.
   final String downUrl;
+
+  /// New features
   final String newFeatures;
+
+  /// App update time description, for example, "released on X."
+  /// The value is calculated by the server and displayed on the client.
   final String releaseDateDesc;
+
+  /// ID of the AppGallery request details API.
   final String detailId;
+
   final String fullDownUrl;
+
   final int bundleSize;
+
+  /// Indicates whether the app is a Huawei-developed app. 
+  /// 
+  /// The options are as follows:
+  /// - **0**: no
+  /// - **1**: yes
   final int devType;
+
+  /// Indicates whether the update is forcible.
   final bool isCompulsoryUpdate;
+
   final bool isAutoUpdate;
+
+  /// Reason for not recommending the update.
   final int notRcmReason;
 
   UpgradeInfo(
